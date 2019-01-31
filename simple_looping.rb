032f365323dca6e5398ab4415c7c000005fd1bf7
sensible-shell-8529 <diana.lee116@gmail.com>
2019-01-31 17:32:57 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -40,6 +40,7 @@ def until_iterator(number_of_times)
   counter =0 
   until counter == number_of_times
   puts phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter +=1
 end
 end
 
